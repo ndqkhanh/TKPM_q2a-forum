c6bf9adc62e825127fd904b31f1046250068ad5c
simplord542 <81230977+simplord542@users.noreply.github.com>
2023-03-26 20:11:50 +0700

    profile screen

--- mobile/src/screens/Profile/UserProfile.js
+++ mobile/src/screens/Profile/UserProfile.js
@@ -19,4 +19,60 @@ import { UserContext } from "~provider/UserProvider";
 import { is_empty, is_URL } from "~utils/string";
 import Post from "~components/Common/Post";
 
+const isCloseToBottom = ({ layoutMeasurement, contentOffset, contentSize }) => {
+  const paddingToBottom = 100;
+  return (
+    layoutMeasurement.height + contentOffset.y >=
+    contentSize.height - paddingToBottom
+  );
+};
 
+const ProfileScreen = ({ navigation, route }) => {
+  const param = route.params;
+  let userId = null;
+  if (param != null) {
+    userId = param.uid;
+  }
+  //console.log("--uid: ", userId)
+  const { userData, setUserData, fetchUserInformation } =
+    useContext(UserContext);
+  //console.log("--user: ", userData)
+  const [pendingData, setPendingData] = useState(userData);
+  // console.log("pendingData", pendingData);
+  const [anotherUserData, setAnotherUserData] = useState({});
+  // const [userData, setUserData] = useState({});
+  const [myQuestionsData, setMyQuestionsData] = useState([]);
+  const [maxLength, setMaxLength] = useState(0);
+  const [page, setPage] = useState(0);
+  const [refetch, setRefetch] = useState(false);
+  const fetchUserProfile = async (userId) => {
+    let data;
+    if (userId != null) {
+      data = await getUserProfile(userId);
+    }
+    // else {
+    //   data = await getMyProfile(token);
+    // }
+    //console.log("--AnotherUser: ", data)
+    if (data) setAnotherUserData(data);
+  };
+  const limit = 5;
+  const fetchMyQuestions = async (page, limit) => {
+    let token = await AsyncStorage.getItem("UserToken");
+    const data = await getMyQuestions(token, page, limit);
+    //console.log("--questions: ", data.questions)
+    var maxLength = 5;
+    try {
+      maxLength = parseInt(data.count);
+    } catch (error) {
+      console.error("error---", error);
+    }
+    setMaxLength(maxLength);
+    setMyQuestionsData((myQuestionsData) => [
+      ...myQuestionsData,
+      ...data.questions,
+    ]);
+    setPage((page) => page + 1);
+    setRefetch(false);
+  };
+  
