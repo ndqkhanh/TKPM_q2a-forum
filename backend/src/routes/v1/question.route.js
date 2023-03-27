2a0853a9539b33ffd8d3231cefcd6ff2f55c2a39
bunrealcua <voduynhan.123@gmail.com>
2023-03-27 23:58:29 +0700

    create question.route.js and post a question route

---
+++ backend/src/routes/v1/question.route.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const auth = require('../../middlewares/auth');
+const validate = require('../../middlewares/validate');
+const questionValidation = require('../../validations/question.validation');
+const questionControlller = require('../../controllers/question.controller');
+const route = express.Router();
+
+route
+  .route('/')
+  .post(auth('createQuestion'), validate(questionValidation.createQuestion), questionControlller.createQuestion);
+
+module.exports = route;
