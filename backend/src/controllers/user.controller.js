528751bf659e71932ccf0ef98233e0957ed15251
Nguyễn Đinh Quang Khánh <20127530@student.hcmus.edu.vn>
2023-03-29 00:10:16 +0700

    build: create user controller

---
+++ backend/src/controllers/user.controller.js
@@ -0,0 +1,19 @@
+const httpStatus = require('http-status');
+const pick = require('../utils/pick');
+const ApiError = require('../utils/ApiError');
+const catchAsync = require('../utils/catchAsync');
+const { userService } = require('../services');
+const { use } = require('passport');
+
+const getUser = catchAsync(async (req, res) => {
+  const user = await userService.getUserById(req.params.userId);
+  if (!user) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'User not found');
+  }
+  delete user.password;
+  res.send(user);
+});
+
+module.exports = {
+  getUser,
+};
