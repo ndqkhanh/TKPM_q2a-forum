6de794d66d05d158e6c59a9ffbd9a5f400e48f07
Nguyễn Đinh Quang Khánh <20127530@student.hcmus.edu.vn>
2023-03-29 00:11:39 +0700

    refactor: fix index controllers

--- backend/src/controllers/index.js
+++ backend/src/controllers/index.js
@@ -1,2 +1,5 @@
 module.exports.authController = require('./auth.controller');
 module.exports.answerController = require('./answer.controller');
+module.exports.questionController = require('./question.controller');
+module.exports.userController = require('./user.controller');
+module.exports.adminController = require('./admin.controller');
