2645fd43142a2eea90214e4f9c1beabc41e5a0e4
Sawashi <kietle234.cc@gmail.com>
2023-03-29 10:25:56 +0700

    Add controller for questions

---
+++ mobile/src/controller/controllQuestion.js
@@ -0,0 +1,35 @@
+import { Alert } from "react-native";
+import {
+  postQuestion,
+  searchQuestion,
+  updateQuestion,
+} from "~services/Question";
+const controllPostQuestion = async (passTitle, passContent) => {
+  try {
+    let message = await postQuestion(passTitle, passContent);
+    Alert.alert(message.header, JSON.stringify(message.content));
+  } catch (error) {
+    return null;
+    // console.log(error);
+    // Alert.alert("error", error);
+  }
+};
+const controllsSearchQuestion = async (keyword, page, limit) => {
+  try {
+    let res = await searchQuestion(keyword, page, limit);
+
+    if (res.hasOwnProperty("questions")) {
+      //alert(JSON.stringify(res));
+      //console.log(res)
+      return res;
+    } else {
+      Alert.alert(message.header, JSON.stringify(message.content));
+      return null;
+    }
+  } catch (error) {
+    return null;
+    // console.log(error);
+    // Alert.alert("error", error);
+  }
+};
+
