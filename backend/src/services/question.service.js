3dcdf4a5b2b9fc4b6d83d2f18cb993ebaf15c277
bunrealcua <voduynhan.123@gmail.com>
2023-03-29 23:13:11 +0700

    create service update question api

--- backend/src/services/question.service.js
+++ backend/src/services/question.service.js
@@ -35,7 +35,76 @@ const deleteQuestionById = async (questionId) => {
   return deleteQuestion;
 };
 
+const countQuestionInDB = async (req) => {
+  const countQuestion = await prisma.questions.count({
+    where: {
+      title: {
+        contains: req.body.keyword,
+      },
+      status: 2,
+    },
+  });
+  return countQuestion;
+};
+
+const searchQuestion = async (req) => {
+  const countQuestions = await prisma.questions.count({});
+  if (req.params.offset > countQuestions / req.params.limit) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'Not Found Questions Related');
+  }
+
+  const listQuestions = await prisma.questions.findMany({
+    skip: req.params.offset * req.params.limit,
+    take: req.params.limit,
+    where: {
+      title: {
+        contains: req.body.keyword,
+      },
+      status: 2,
+    },
+  });
+
+  if (!listQuestions) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'There is no questions related to keywords');
+  }
+
+  let data = [];
+
+  for (let i = 0; i < listQuestions.length; i++) {
+    const user = await prisma.users.findUnique({
+      where: {
+        id: listQuestions[i].uid,
+      },
+    });
+
+    const answers = await prisma.answers.findMany({
+      where: {
+        qid: listQuestions[i].id,
+      },
+    });
+
+    let check = false;
+
+    for (let j = 0; j < answers.length; j++) {
+      if (answers[j].correct == true) check = true;
+    }
+
+    data.push({
+      questionData: listQuestions[i],
+      numOfAnswers: answers.length,
+      correctAnswerExists: check,
+      userData: {
+        name: user.name,
+        profilepictureurl: user.profilepictureurl,
+      },
+    });
+  }
+  return data;
+};
+
 module.exports = {
   createQuestion,
   deleteQuestionById,
+  countQuestionInDB,
+  searchQuestion,
 };
