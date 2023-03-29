6a6c26c4d5c0245089408268d375bc5ccd5c81c8
Sawashi <kietle234.cc@gmail.com>
2023-03-29 10:37:12 +0700

    Add configs

---
+++ mobile/src/provider/ConfigProvider.js
@@ -0,0 +1,33 @@
+import React, { useContext, useEffect, useState } from "react";
+import { UserContext } from "~provider/UserProvider";
+import { getConfiguration } from "~services/getConfig";
+
+const ConfigContext = React.createContext();
+
+ConfigContext.displayName = "ConfigContext";
+
+const ConfigProvider = ({ children }) => {
+  const { auth, userData } = useContext(UserContext);
+  const [configData, setConfigData] = useState(null);
+  const fetchConfigInformation = async () => {
+    let data = await getConfiguration();
+
+    if (data) {
+      setConfigData(data);
+    }
+  };
+
+  return (
+    <ConfigContext.Provider
+      value={{
+        configData,
+        setConfigData,
+        fetchConfigInformation,
+      }}
+    >
+      {children}
+    </ConfigContext.Provider>
+  );
+};
+
+export { ConfigProvider, ConfigContext };
