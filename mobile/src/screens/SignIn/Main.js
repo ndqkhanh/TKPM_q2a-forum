8b5b347c4a5b66eba6d9140bfba9294d08880bc4
simplord542 <81230977+simplord542@users.noreply.github.com>
2023-03-30 14:45:49 +0700

    import lib for signin

---
+++ mobile/src/screens/SignIn/Main.js
@@ -0,0 +1,43 @@
+import React, { useContext, useEffect, useState } from "react";
+import {
+  Dimensions,
+  Image,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  TouchableOpacity,
+  Alert,
+} from "react-native";
+import { Colors } from "react-native-ui-lib";
+import Icon from "react-native-vector-icons/Ionicons";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { API_URL } from "@env";
+import { ScrollView } from "react-native-gesture-handler";
+import { UserContext } from "~provider/UserProvider";
+
+f
+f
+f
+f
+f
+f
+f
+f
+f
+f
+f
+f
+f
+f
+f
+f
+f
+f
+ff
+f
+f
+f
+f
+f
