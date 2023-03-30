75fff504e1e6849fee315efc3b150cae51df2dc5
Nguyễn Đinh Quang Khánh <20127530@student.hcmus.edu.vn>
2023-03-30 09:49:28 +0700

    add services for delete and update answer

--- backend/src/services/answer.service.js
+++ backend/src/services/answer.service.js
@@ -3,6 +3,51 @@ const { PrismaClient, Prisma } = require('@prisma/client');
 const prisma = new PrismaClient();
 const ApiError = require('../utils/ApiError');
 
+const createAnswer = async (req) => {
+  const checkQuestionExists = await prisma.questions.findUnique({
+    where: {
+      id: req.body.qid,
+    },
+  });
+  if (!checkQuestionExists) {
+    throw new ApiError(httpStatus.BAD_REQUEST, 'Question does not exist');
+  }
+  // Check if the question is approved
+  if (checkQuestionExists.status !== 2) {
+    throw new ApiError(httpStatus.BAD_REQUEST, 'Question is not approved');
+  }
+  const userid = req.user.id;
+  const answer = prisma.answers.create({
+    data: {
+      qid: req.body.qid,
+      content: req.body.content,
+      uid: userid,
+    },
+  });
+  return answer;
+};
+
+const changeAnswer = async (req) => {
+  const checkAnswerExists = await prisma.answers.findUnique({
+    where: {
+      id: req.params.answerId,
+    },
+  });
+  if (!checkAnswerExists) {
+    throw new ApiError(httpStatus.BAD_REQUEST, 'Answer does not exist');
+  }
+  const answer = prisma.answers.update({
+    where: {
+      id: req.params.answerId,
+    },
+    data: {
+      content: req.body.content,
+      updated_at: new Date(),
+    },
+  });
+  return answer;
+};
+
 const pickCorrectAnswerById = async (req) => {
   const checkAnswerExists = await prisma.answers.findUnique({
     where: {
@@ -67,6 +112,29 @@ const pickCorrectAnswerById = async (req) => {
   return answer;
 };
 
+const delAnswerById = async (answerId) => {
+  const checkAnswerExists = await prisma.answers.findUnique({
+    where: {
+      id: answerId,
+    },
+  });
+  if (!checkAnswerExists) {
+    return null;
+  }
+  let deleteAnswer = null;
+  if (checkAnswerExists) {
+    deleteAnswer = await prisma.answers.delete({
+      where: {
+        id: answerId,
+      },
+    });
+  }
+  return deleteAnswer;
+};
+
 module.exports = {
+  createAnswer,
+  changeAnswer,
+  delAnswerById,
   pickCorrectAnswerById,
 };
