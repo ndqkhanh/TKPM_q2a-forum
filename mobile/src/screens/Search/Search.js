4e5387325d66a37c90b636ae53d9f42c08d6c9cf
simplord542 <81230977+simplord542@users.noreply.github.com>
2023-03-30 12:43:22 +0700

    search screen

--- mobile/src/screens/Search/Search.js
+++ mobile/src/screens/Search/Search.js
@@ -16,25 +16,36 @@ import SearchBar from "~components/SearchBar/SearchBar";
 import { formatDistance } from "date-fns";
 import { ConfigContext } from "~provider/ConfigProvider";
 import { is_empty } from "~utils/string";
+const SearchScreen = ({ navigation }) => {
+  const { configData } = React.useContext(ConfigContext);
+  const [titleSearch, setTitleSearch] = React.useState("");
+  const [countRes, setCountRes] = React.useState(null);
+  const [searchData, setSearchData] = React.useState([]);
+  const limit = 5;
+  const [page, setPage] = React.useState(1);
+  const pressNext = () => {
+    if (page < Math.ceil(countRes / limit)) {
+      let newPage = page + 1;
+      setPage(newPage);
+      if (Math.ceil(searchData.length / limit) < newPage)
+        getData(false, newPage - 1, limit);
+    }
+  };
+  const pressPrev = () => {
+    if (page > 1) setPage(page - 1);
+  };
+  const getData = async (newSearch, page, limit) => {
+    try {
+      let res = await controllsSearchQuestion(titleSearch, page, limit);
+      setSearchData([]);
+      if (res != null) {
+        setCountRes(parseInt(res.count));
+        if (newSearch == true) setSearchData([...res.questions]);
+        else setSearchData([...searchData, ...res.questions]);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-
-h
-h
-h
-h
-h
-h
-h
-h
-h
-h
-h
-hh
-h
-h
-h
-h
-h
-h
-h
 
