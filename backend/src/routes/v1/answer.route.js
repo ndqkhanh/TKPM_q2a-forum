f8a57e95d08a37b87046177d7d7d06b25d06c69a
Nguyễn Đinh Quang Khánh <20127530@student.hcmus.edu.vn>
2023-03-30 09:47:13 +0700

    add answer route for delete and update

--- backend/src/routes/v1/answer.route.js
+++ backend/src/routes/v1/answer.route.js
@@ -9,8 +9,12 @@ const router = express.Router();
 router.route('/').post(auth('createAnswer'), validate(ansValidation.newAnswer), ansController.newAnswer);
 
 router
+  .route('/:answerId')
+  .post(auth('updateAnswer'), validate(ansValidation.updateAnswer), ansController.updateAnswer)
+  .delete(auth('deleteAnswer'), validate(ansValidation.deleteAnswer), ansController.deleteAnswer);
+
+router
   .route('/:answerId/pick-correct')
   .post(auth('pickCorrectAnswer'), validate(ansValidation.pickCorrectAnswer), ansController.pickCorrectAnswer);
-
 
 module.exports = router;
