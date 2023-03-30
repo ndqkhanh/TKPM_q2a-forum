c8a777f85add24fbdd14243d73c1489c157534a8
simplord542 <81230977+simplord542@users.noreply.github.com>
2023-03-30 12:20:29 +0700

    import for search

---
+++ mobile/src/screens/Search/Search.js
@@ -0,0 +1,40 @@
+import * as React from "react";
+import {
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+} from "react-native";
+import { Colors } from "react-native-ui-lib";
+import Icon from "react-native-vector-icons/Ionicons";
+import { controllsSearchQuestion } from "~controller/controllQuestion";
+import Q2APagination from "~components/Q2A/Pagination";
+import Post from "~components/Common/Post";
+import SearchBar from "~components/SearchBar/SearchBar";
+import { formatDistance } from "date-fns";
+import { ConfigContext } from "~provider/ConfigProvider";
+import { is_empty } from "~utils/string";
+
+
+h
+h
+h
+h
+h
+h
+h
+h
+h
+h
+h
+hh
+h
+h
+h
+h
+h
+h
+h
+
