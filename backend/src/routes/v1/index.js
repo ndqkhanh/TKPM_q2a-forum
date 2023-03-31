17c244cccdcff54c19045d4a22f9125d03c6f092
Nguyễn Đinh Quang Khánh <20127530@student.hcmus.edu.vn>
2023-03-31 18:08:56 +0700

    fix index.js role

--- backend/src/routes/v1/index.js
+++ backend/src/routes/v1/index.js
@@ -2,44 +2,49 @@ const express = require('express');
 const authRoute = require('./auth.route');
 const answerRoute = require('./answer.route');
 const questionRoute = require('./question.route');
+const adminRoute = require('./admin.route');
 const userRoute = require('./user.route');
 const router = express.Router();
 
 const defaultRoutes = [
-    {
-      path: '/auth',
-      route: authRoute,
-    },
-    {
-      path: '/user',
-      route: userRoute,
-    },
-    {
+  {
+    path: '/auth',
+    route: authRoute,
+  },
+  {
+    path: '/user',
+    route: userRoute,
+  },
+  {
     path: '/answer',
     route: answerRoute,
-    },
-    {
-      path: '/question',  
-      route: questionRoute,
-    }
+  },
+  {
+    path: '/question',
+    route: questionRoute,
+  },
+  {
+    path: '/admin',
+    route: adminRoute,
+  }
 ];
 
 const devRoutes = [
-    // routes available only in development mode
-    {
-      path: '/docs',
-      route: docsRoute,
-    },
-  ];
-  
-  defaultRoutes.forEach((route) => {
+  // routes available only in development mode
+  {
+    path: '/docs',
+    route: docsRoute,
+  },
+];
+
+defaultRoutes.forEach((route) => {
+  router.use(route.path, route.route);
+});
+
+/* istanbul ignore next */
+if (config.env === 'development') {
+  devRoutes.forEach((route) => {
     router.use(route.path, route.route);
   });
-  
-  /* istanbul ignore next */
-  if (config.env === 'development') {
-    devRoutes.forEach((route) => {
-      router.use(route.path, route.route);
-    });
-  }
+}
 module.exports = router;
