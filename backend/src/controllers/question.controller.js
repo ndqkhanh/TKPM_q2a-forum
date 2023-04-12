20fa4e2f83c3db7f2f799f60fcb9df2de091fd17
Nguyễn Đinh Quang Khánh <20127530@student.hcmus.edu.vn>
2023-04-12 14:47:50 +0700

    fix index.js of controller

--- backend/src/controllers/question.controller.js
+++ backend/src/controllers/question.controller.js
@@ -1,6 +1,5 @@
 const httpStatus = require('http-status');
 const catchAsync = require('../utils/catchAsync');
-const ApiError = require('../utils/ApiError');
 const questionService = require('../services/question.service');
 
 const createQuestion = catchAsync(async (req, res) => {
