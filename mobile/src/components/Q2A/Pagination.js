9e35648397477a1b5a31e1ffcc2f9901dc4b278f
Nguyễn Đinh Quang Khánh <20127530@student.hcmus.edu.vn>
2023-05-20 20:37:32 +0700

    build: fix something

--- mobile/src/components/Q2A/Pagination.js
+++ mobile/src/components/Q2A/Pagination.js
@@ -25,37 +25,37 @@ const Q2APagination = ({ page, maxPage, pressPrev, pressNext }) => {
   );
 };
 const styles = StyleSheet.create({
-    container: {
-        flexDirection: "row",
-        justifyContent: "space-between",
-        marginTop: 20,
-    },
-    btnPage: {
-        backgroundColor: Colors.blue40,
-        paddingVertical: 5,
-        width: 100,
-        justifyContent: "center",
-        alignItems: "center",
-        borderRadius: 5,
-    },
-    btnTextPage: {
-        fontWeight: "bold",
-        fontSize: 15,
-        color: Colors.white,
-    },
-    pageContainer: {
-        backgroundColor: Colors.blue20,
-        borderRadius: 5,
-        paddingVertical: 5,
-        width: 60,
-        justifyContent: "center",
-        alignItems: "center",
-    },
-    pageNumber: {
-        fontWeight: "bold",
-        fontSize: 15,
-        color: Colors.white,
-    },
+  container: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: 20,
+  },
+  btnPage: {
+    backgroundColor: Colors.blue40,
+    paddingVertical: 5,
+    width: 100,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 5,
+  },
+  btnTextPage: {
+    fontWeight: "bold",
+    fontSize: 15,
+    color: Colors.white,
+  },
+  pageContainer: {
+    backgroundColor: Colors.blue20,
+    borderRadius: 5,
+    paddingVertical: 5,
+    width: 60,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  pageNumber: {
+    fontWeight: "bold",
+    fontSize: 15,
+    color: Colors.white,
+  },
 });
 
 export default Q2APagination;
